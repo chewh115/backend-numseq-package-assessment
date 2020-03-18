db82f5b9119f49cc148aca7b1784d24c929d9b45
Chelsea <chewh115@gmail.com>
2020-03-18 15:09:17 -0400

    Updates to have working solutions for all geo.py functions

---
+++ numseq/geo.py
@@ -0,0 +1,13 @@
+def sqaure(n):
+    """Returns nth term of numbers that can be arranged in geo squares"""
+    return n**2
+
+
+def triangle(n):
+    """Returns nth term of numbers that can be arranged in geo triangle"""
+    return sum(range(1, n+1))
+
+
+def cube(n):
+    """Returns nth term of numbers that can be arranged in geo cube"""
+    return n**3
